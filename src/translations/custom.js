b7dd59c06a3c26fa8569e98b0bf720498514e792
Clément Gayot <contact@clement-gayot.fr>
2020-10-21 10:28:13 +0200

    :sparkles: :wrench: Add klaro custom build, update webpack config (remove babel compilation)

---
+++ src/translations/custom.js
@@ -0,0 +1,10 @@
+// To create a new translation, add a YAML file with the required entries and
+// link it here using the appropriate language code.
+
+import en from './en.yml';
+import fr from './fr.yml';
+
+export default {
+    en,
+    fr,
+};
